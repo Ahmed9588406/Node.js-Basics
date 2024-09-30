8ccadb3dd50fc42706c177040ed5383c40760d41
Ahmed9588406 <ak9588406@gmail.com>
2024-09-30 18:26:15 +0300

    eventemitter added

--- app.js
+++ app.js
@@ -1,7 +1,19 @@
-const tutorial = require("./tutorial")
+/*const tutorial = require("./tutorial")
 
 console.log(tutorial)
 
 console.log(tutorial.sum(1,1))
 console.log(tutorial.PI)
 console.log(new tutorial.SomeMathObject())
+*/
+
+
+const EventEmitter = require('events')
+
+const eventemitter = new EventEmitter()
+
+eventemitter.on('tutorial',()=>{
+    console.log("tutorial event has occured")
+});
+
+eventemitter.emit('tutorial')
